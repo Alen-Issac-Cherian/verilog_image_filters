4a3b9563d24787c8bb0cde2f41366e302d4f6cee
Alen-Issac-Cherian <60470499+Alen-Issac-Cherian@users.noreply.github.com>
2021-03-30 13:35:45 +0530

    Update sobel_filter.v
    Commented on the working of the module

--- sobel_filter.v
+++ sobel_filter.v
@@ -1,17 +1,27 @@
-
-
+/*
+Working of this module:
+It uses two reg matrices, one to store the image of resolution n x m pixels in one frame and the other to select the required pixels for the convolution. 
+Here one pixel is considered to be of 1 byte wide. To do this, the entire operation is divided into 3 subtasks:-
+a) storage of the n x m pixels
+b) selecting the 9 bytes needed to perform the sobel filter convolution
+c) performing the 3 x 3 sobel convolution
+This cycle repeats for every subsequent frames. A FSM with 4 states is defined to sequence these subtasks correctly.
+*/
 module sobel_filter(
     input [7:0] Din,
-    input data_valid, rst, clk,
-    output fill_now,
-    output [15:0] Dout
+    input data_valid, clk,
+    input rst,  //external active high asynchronous reset
+    output fill_now,    //status signal to indicate whether the first reg matrix is empty or not
+    output [15:0] Dout  
 );
 
 reg [15:0] result;
 
-parameter N = 720, M = 1280;
+parameter N = 720, M = 1280;    //resolution of the image
 
-reg [7:0] storage[0:N-1][0:M-1];
+//reg matrix to store the image pixels in a frame of operation
+reg [7:0] storage[0:N-1][0:M-1];     
+//3 x 3 reg matrix to house the 9 bytes for the sobel convolution
 reg [7:0] image_kernal[0:2][0:2];
 
 integer r, c, i, j;
@@ -23,7 +33,7 @@ reg [1:0] PS, NS;
 always @(posedge clk or posedge rst)
 begin
  if (rst) 
-  PS <= IDLE;    
+  PS <= IDLE;    //every value is reset to its default value in IDLE
  else PS <= NS;    
 end
 
@@ -58,14 +68,14 @@ begin
          if((r == N-1) && (c == M-1))
          begin
           storage[r][c] <= Din;
-          NS <= FIX;
+          NS <= FIX;    //fill the last byte of the matrix and go to next state
          end
          else if(c == M-1)
           begin
            storage[r][c] <= Din;
            c <= 0;
            r <= r+1;
-           NS <= STORE;
+           NS <= STORE; //fill the last byte of current row and remain in this state
           end
          else 
          begin
@@ -80,26 +90,26 @@ begin
          for(c = j, c < j+3, c = c+1) 
           image_kernal[r][c] <= storage[r][c];
         end
-       NS <= CONVOLUTE;
+       NS <= CONVOLUTE; //fill the image kernal with that of storage. The starting position is specified by i and j.
       end
  CONVOLUTE: begin
              if((i == N-3) && (j == M-3))
              begin
               result <= image_kernal[0][0] + (2*image_kernal[0][1]) + image_kernal[0][2] - image_kernal[2][0] - (2*image_kernal[2][1]) - image_kernal[2][2];
-              NS <= IDLE;
+              NS <= IDLE; //perform the last convolution in the current frame and go to IDLE
              end
              else if(c == M-3)
               begin
                result <= image_kernal[0][0] + (2*image_kernal[0][1]) + image_kernal[0][2] - image_kernal[2][0] - (2*image_kernal[2][1]) - image_kernal[2][2];
                j <= 0;
                i <= i+1;
-               NS <= FIX;
+               NS <= FIX; //perform the last convolution in the current horizontal direction. Move one row down and go to FIX to take the next 9 pixels.
               end
              else 
               begin
                result <= image_kernal[0][0] + (2*image_kernal[0][1]) + image_kernal[0][2] - image_kernal[2][0] - (2*image_kernal[2][1]) - image_kernal[2][2];
                j <= j+1;
-               NS <= FIX;
+               NS <= FIX; //perform the covolution. Move one column right and go to FIX to take the next 9 pixels.
               end
             end
  default: PS <= IDLE;
@@ -107,6 +117,8 @@ begin
 end 
 
 assign fill_now = ((PS==FIX) || (PS==CONVOLUTE)) ? 1'b0 : 1'b1;
+//storage is full when PS is in FIX nd CONVOLUTE states.
 assign Dout = (PS==CONVOLUTE) ? result : 16'hzzzz;
+//output data is available when PS is in CONVOLUTE state.
 
 endmodule 
